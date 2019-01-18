b63569581e750d656c599f233105a56c28659409
Watit Thammarat <tongnakub@hotmail.com>
2019-01-19 02:49:55 +0700

    init

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class HomeController < ApplicationController
+  def index
+    @number_of_recipes = Recipe.count
+  end
+
+  def about; end
+end
---
+++ test.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+result = 0
+puts 'Hello World 1' if result
+
+result = false
+puts 'Hello World 2' if result
